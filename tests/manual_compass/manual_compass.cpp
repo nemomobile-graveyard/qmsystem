794f3d790b246b14cba7c296da4ef58ec5cf3f5f
yang yang <ext-yang.yang25@nokia.com>
2011-04-08 10:56:13 +0300

    add declination test for qmcompass

--- tests/manual_compass/manual_compass.cpp
+++ tests/manual_compass/manual_compass.cpp
@@ -30,6 +30,7 @@
 #include <QList>
 #include <QTime>
 #include <cstdio>
+#include <gconf/gconf-client.h>
 
 #define TOLERANCE_AVG 15
 
@@ -42,6 +43,7 @@ class TestClass : public QObject
 public slots:
 
     void compassChanged(const MeeGo::QmCompassReading newDegrees) {
+        //printf("\tSesnor NO declination : %d\n", newDegrees.degrees);
         if (readLevel) {
             level = newDegrees.level;
             printf("\t Calibration level: %d\n", newDegrees.level);
@@ -61,6 +63,12 @@ public slots:
         }
     }
 
+    void compassDecChanged(const MeeGo::QmCompassReading newDegrees) {
+        count++;
+        //printf("\tSesnor YES declination : %d\n", newDegrees.degrees);
+
+    }
+
 private:
 
     void calibrate() {
@@ -78,7 +86,6 @@ private:
             if (level >= 3) {
                 break;
             }
-            //printf("\tlevel: %d\n", sensor->level());
         }
         readLevel = false;
         printf("\nRequired calibration level reached.\n");
@@ -122,18 +129,58 @@ private:
 
 private slots:
     void initTestCase() {
+        count = 0;
         readLevel = false;
         readCompass = false;
         printCompass = false;
         level = 0;
         sensor = new MeeGo::QmCompass();
+        sensorDec = new MeeGo::QmCompass();
         QVERIFY(sensor);
+        QVERIFY(sensorDec);
         QVERIFY(connect(sensor, SIGNAL(dataAvailable(const MeeGo::QmCompassReading)),
                 this, SLOT(compassChanged(const MeeGo::QmCompassReading))));
+        QVERIFY(connect(sensorDec, SIGNAL(dataAvailable(const MeeGo::QmCompassReading)),
+                this, SLOT(compassDecChanged(const MeeGo::QmCompassReading))));
         QVERIFY2(sensor->requestSession(MeeGo::QmSensor::SessionTypeControl) != MeeGo::QmSensor::SessionTypeNone,
                 sensor->lastError().toLocal8Bit());
+        QVERIFY2(sensorDec->requestSession(MeeGo::QmSensor::SessionTypeControl) != MeeGo::QmSensor::SessionTypeNone,
+                sensorDec->lastError().toLocal8Bit());
         sensor->setStandbyOverride(true);
-        QVERIFY2(sensor->start(), sensor->lastError().toLocal8Bit());
+        sensorDec->setStandbyOverride(true);
+        QVERIFY2(sensor->start(), sensor->lastError().toLocal8Bit());        
+        QVERIFY2(sensorDec->start(), sensorDec->lastError().toLocal8Bit());
+        ep.start();
+        g_type_init();
+        gcClient = gconf_client_get_default();
+    }
+
+    void testDeclination() {
+
+        printf("Test compass declination\n");
+        printf("Calibrating sensors. Please rotate....\n\n");
+        while(1) {
+            if (sensorDec->get().level >= 3 && sensorDec->get().level >= 3)
+                printf("SensorDec calibration level is: 3\n\n");
+                break;
+        }
+        sensorDec->setUseDeclination(true);
+        QVERIFY(sensorDec->useDeclination());
+        printf("SensorDec is using declination\n");
+        sensor->setUseDeclination(false);
+        QVERIFY(!sensor->useDeclination());
+        printf("Sensor is NOT using declination\n");
+
+        gconf_client_set_int(gcClient, "/system/osso/location/settings/magneticvariation", 10, NULL);
+        QVERIFY(gconf_client_get_int(gcClient, "/system/osso/location/settings/magneticvariation", NULL) == 10);
+
+        printf("Measuring...Move the device\n\n");
+        QTest::qWait(10000);
+        printf("SensorDec: %d, Sensor: %d\n", sensorDec->get().degrees, sensor->get().degrees);
+        //QVERIFY(sensorDec->get().degrees - sensor->get().degrees == 10);
+        printf("SensorDec interval %d\n\n", sensorDec->interval());
+        printf("Time elapsed %d\n\n", ep.elapsed());
+        printf("Received data reading %d\n\n", count);
     }
 
     void testManual() {
@@ -156,11 +203,15 @@ private slots:
 
     void cleanupTestCase() {
         QVERIFY2(sensor->stop(), sensor->lastError().toLocal8Bit());
+        QVERIFY2(sensorDec->stop(), sensorDec->lastError().toLocal8Bit());
         delete sensor;
+        delete sensorDec;
     }
 
 private:
     MeeGo::QmCompass *sensor;
+    MeeGo::QmCompass *sensorDec;
+    GConfClient *gcClient;
 
     bool readCompass;
     bool printCompass;
@@ -169,6 +220,8 @@ private:
 
     bool readLevel;
     int level;
+    int count;
+    QTime ep;
 
 };
 
