167ff78f78cfcd03d1f0e0e3bc4262256c1fc622
Matias Muhonen <ext-matias.muhonen@nokia.com>
2011-10-04 11:04:48 +0300

    Implement API to check the PR1.x version

--- system/qmsysteminformation.h
+++ system/qmsysteminformation.h
@@ -8,7 +8,7 @@
 
    @author Matias Muhonen <ext-matias.muhonen@nokia.com>
 
-   @scope Private
+   @scope Nokia Meego
 
    This file is part of SystemSW QtAPI.
 
@@ -36,11 +36,92 @@ namespace MeeGo {
 
 class QmSystemInformationPrivate;
 
+/*!
+ *
+ * @scope Nokia Meego
+ *
+ * This class can be used to retrieve information from the system information service.
+ *
+ * The retrieve the version of the operating system, see the following example:
+ *
+
+\code
+#include <qmsysteminformation.h>
+
+MeeGo::QmSystemInformation::OperatingSystemVersion release =
+    MeeGo::QmSystemInformation::operatingSystemVersion();
+
+if (release >= MeeGo::QmSystemInformation::OSVersion_Harmattan_1_2) {
+    // It is PR1.2
+}
+
+\endcode
+
+ */
+
 class MEEGO_SYSTEM_EXPORT QmSystemInformation : public QObject
 {
     Q_OBJECT
 
 public:
+    /*!
+     * Represents the version of the operating system.
+     */
+    enum OperatingSystemVersion {
+        /**
+         * Unknown version
+         */
+        OSVersion_Unknown          = 0,
+        /**
+         * Version 1.0
+         */
+        OSVersion_Harmattan_1_0    = 100,
+        /**
+         * Version 1.1
+         */
+        OSVersion_Harmattan_1_1    = 110,
+        /**
+         * Version 1.1.1
+         */
+        OSVersion_Harmattan_1_1_1  = 111,
+        /**
+         * Version 1.2
+         */
+        OSVersion_Harmattan_1_2    = 120
+    };
+
+    /*!
+     * @brief Gets the operating system version.
+     * @param firmwareVersion Optional: the firmware version string. If empty, the firmware version is retrieved from the system information service.
+     * @return Returns the operating system version.
+     */
+    static inline QmSystemInformation::OperatingSystemVersion operatingSystemVersion(QString firmwareVersion = "")
+    {
+        if (firmwareVersion.isEmpty()) {
+            MeeGo::QmSystemInformation systemInformation;
+            firmwareVersion = systemInformation.valueForKey("/device/sw-release-ver");
+        }
+
+        if (!firmwareVersion.startsWith("DFL61_HARMATTAN_") || firmwareVersion.length() < 20) {
+            return OSVersion_Unknown;
+        }
+
+        int majorVersion = firmwareVersion.at(16).digitValue();
+        int minorVersion = firmwareVersion.at(17).digitValue();
+
+        if (majorVersion >= 3) {
+            return OSVersion_Harmattan_1_2;
+        } else if (majorVersion == 2 && minorVersion >= 2) {
+            return OSVersion_Harmattan_1_1_1;
+        } else if (majorVersion == 2) {
+            return OSVersion_Harmattan_1_1;
+        } else if (majorVersion == 1) {
+            return OSVersion_Harmattan_1_0;
+        }
+
+        return OSVersion_Unknown;
+    }
+
     QmSystemInformation();
     virtual ~QmSystemInformation();
 
--- tests/systeminformation/systeminformation.cpp
+++ tests/systeminformation/systeminformation.cpp
@@ -3,7 +3,7 @@
  * @brief System Information tests.
 
    <p>
-   Copyright (C) 2009-2010 Nokia Corporation
+   Copyright (C) 2009-2011 Nokia Corporation
 
    @author Matias Muhonen <ext-matias.muhonen@nokia.com>
 
@@ -47,6 +47,72 @@ private slots:
         /* SysInfo data is static, so we must get the same value every time we query a key */
         QVERIFY(v1.compare(v2) == 0);
         QVERIFY(v1.compare(v3) == 0);
+    }
+
+    void testOperatingSystemPr10() {
+        MeeGo::QmSystemInformation::OperatingSystemVersion v =
+             MeeGo::QmSystemInformation::operatingSystemVersion("DFL61_HARMATTAN_10.2011.34-1_PR_LEGACY_001_ARM.bin");
+
+        QVERIFY(v == MeeGo::QmSystemInformation::OSVersion_Harmattan_1_0);
+
+        MeeGo::QmSystemInformation::OperatingSystemVersion v2 =
+             MeeGo::QmSystemInformation::operatingSystemVersion("DFL61_HARMATTAN_1.2011.34-1_PR_LEGACY_001_ARM.bin");
+
+        QVERIFY(v2 == MeeGo::QmSystemInformation::OSVersion_Harmattan_1_0);
+    }
+
+    void testOperatingSystemPr11() {
+        MeeGo::QmSystemInformation::OperatingSystemVersion v =
+             MeeGo::QmSystemInformation::operatingSystemVersion("DFL61_HARMATTAN_20.2011.34-1_PR_LEGACY_001_ARM.bin");
+
+        QVERIFY(v == MeeGo::QmSystemInformation::OSVersion_Harmattan_1_1);
+
+        MeeGo::QmSystemInformation::OperatingSystemVersion v2 =
+             MeeGo::QmSystemInformation::operatingSystemVersion("DFL61_HARMATTAN_2.2011.34-1_PR_LEGACY_001_ARM.bin");
+
+        QVERIFY(v2 == MeeGo::QmSystemInformation::OSVersion_Harmattan_1_1);
+    }
+
+    void testOperatingSystemPr111() {
+        MeeGo::QmSystemInformation::OperatingSystemVersion v =
+             MeeGo::QmSystemInformation::operatingSystemVersion("DFL61_HARMATTAN_22.2011.34-1_PR_LEGACY_001_ARM.bin");
+
+        QVERIFY(v == MeeGo::QmSystemInformation::OSVersion_Harmattan_1_1_1);
+    }
+
+    void testOperatingSystemPr12() {
+        MeeGo::QmSystemInformation::OperatingSystemVersion v =
+             MeeGo::QmSystemInformation::operatingSystemVersion("DFL61_HARMATTAN_30.2011.34-1_PR_LEGACY_001_ARM.bin");
+
+        QVERIFY(v == MeeGo::QmSystemInformation::OSVersion_Harmattan_1_2);
+
+        MeeGo::QmSystemInformation::OperatingSystemVersion v2 =
+             MeeGo::QmSystemInformation::operatingSystemVersion("DFL61_HARMATTAN_3.2011.34-1_PR_LEGACY_001_ARM.bin");
+
+        QVERIFY(v2 == MeeGo::QmSystemInformation::OSVersion_Harmattan_1_2);
+
+       MeeGo::QmSystemInformation::OperatingSystemVersion v3 =
+             MeeGo::QmSystemInformation::operatingSystemVersion("DFL61_HARMATTAN_40.2011.34-1_PR_LEGACY_001_ARM.bin");
+
+        QVERIFY(v3 == MeeGo::QmSystemInformation::OSVersion_Harmattan_1_2);
+    }
+
+    void testInvalidOperatingSystemVersion() {
+        MeeGo::QmSystemInformation::OperatingSystemVersion v =
+             MeeGo::QmSystemInformation::operatingSystemVersion("DFL61_HARMATTAN_DIIBADAABA.bin");
+
+        QVERIFY(v == MeeGo::QmSystemInformation::OSVersion_Unknown);
+    }
+
+    void testOperatingSystemVersion() {
+        MeeGo::QmSystemInformation::OperatingSystemVersion v =
+            MeeGo::QmSystemInformation::operatingSystemVersion();
+
+        QVERIFY(v > MeeGo::QmSystemInformation::OSVersion_Unknown);
+        QVERIFY(v > MeeGo::QmSystemInformation::OSVersion_Harmattan_1_0);
+        QVERIFY(v > MeeGo::QmSystemInformation::OSVersion_Harmattan_1_1);
+        QVERIFY(v > MeeGo::QmSystemInformation::OSVersion_Harmattan_1_1_1);
+        QVERIFY(v >= MeeGo::QmSystemInformation::OSVersion_Harmattan_1_2);
     }
 };
 
